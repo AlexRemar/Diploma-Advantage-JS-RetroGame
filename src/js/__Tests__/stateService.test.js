500ed25547527d5c3ed764fbc47ba00b1968d8ea
Alex Remar <takoe.moscow@gmail.com>
2023-08-03 20:21:11 +0300

    Initial Commit

---
+++ src/js/__Tests__/stateService.test.js
@@ -0,0 +1,8 @@
+import GameStateService from '../GameStateService';
+
+test('Должна выбросить исключение при невозможности загрузить игру', () => {
+  const stateService = new GameStateService(null);
+  const received = () => stateService.load();
+  expect(received).toThrow('Invalid state');
+});
+
